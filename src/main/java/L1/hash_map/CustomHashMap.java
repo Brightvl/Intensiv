978fc9b07b5f1c6d09f2c095b1761634c0c4a55b
Iaroslav S <ziro-x-100@mail.ru>
2024-03-02 15:29:07 +0300

    realize CustomHashMap

--- src/main/java/L1/hash_map/CustomHashMap.java
+++ src/main/java/L1/hash_map/CustomHashMap.java
@@ -1,9 +1,36 @@
 package L1.hash_map;
 
+import java.util.LinkedList;
+
 /**
  * Своя реализация CustomHashMap
- *
- * @param <E>
  */
-public class CustomHashMap<E> {
+public class CustomHashMap<K, V> {
+
+    private static final int DEFAULT_CAPACITY = 16;
+    private LinkedList<Entry<K, V>>[] buckets;
+
+    public CustomHashMap() {
+        buckets = new LinkedList[DEFAULT_CAPACITY];
+        for (int i = 0; i < DEFAULT_CAPACITY; i++) {
+            buckets[i] = new LinkedList<>();
+        }
+    }
+
+    public void put(K key, V value) {
+
+    }
+
+    public V get(K kye) {
+        throw new RuntimeException();
+    }
+
+    public void remove(K key) {
+
+    }
+
+    public int getHash(K key) {
+        throw new RuntimeException();
+    }
+
 }
