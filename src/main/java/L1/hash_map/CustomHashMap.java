da0a40e8ec55a2d5a72a31f6432946a4691a9513
Iaroslav S <ziro-x-100@mail.ru>
2024-03-04 14:58:16 +0300

    realize CustomHashMap

--- src/main/java/L1/hash_map/CustomHashMap.java
+++ src/main/java/L1/hash_map/CustomHashMap.java
@@ -8,7 +8,7 @@ import java.util.LinkedList;
 public class CustomHashMap<K, V> {
 
     private static final int DEFAULT_CAPACITY = 16;
-    private LinkedList<Entry<K, V>>[] buckets;
+    private final LinkedList<Entry<K, V>>[] buckets;
 
     public CustomHashMap() {
         buckets = new LinkedList[DEFAULT_CAPACITY];
@@ -17,10 +17,33 @@ public class CustomHashMap<K, V> {
         }
     }
 
+    /**
+     * Добавляет значение по ключу в словарь
+     *
+     * @param key   ключ
+     * @param value значение
+     */
     public void put(K key, V value) {
+        int index = getHash(key) % DEFAULT_CAPACITY;
+        LinkedList<Entry<K, V>> bucket = buckets[index];
 
+        for (Entry<K, V> entry : bucket) {
+            if (entry.getKey().equals(key)) {
+                entry.setValue(value);
+                return;
+            }
+        }
+
+        bucket.add(new Entry<>(key, value));
     }
 
+
+    /**
+     * Получить значение по ключу
+     *
+     * @param key ключ
+     * @return значение словаря
+     */
     public V get(K key) {
         int index = getHash(key) % DEFAULT_CAPACITY;
         LinkedList<Entry<K, V>> bucket = buckets[index];
@@ -31,15 +54,40 @@ public class CustomHashMap<K, V> {
             }
         }
 
-        return null;
+        return null; // Key not found
     }
 
+
+    /**
+     * Удаляет значение по ключу
+     *
+     * @param key ключ
+     */
     public void remove(K key) {
+        int index = getHash(key) % DEFAULT_CAPACITY;
+        LinkedList<Entry<K, V>> bucket = buckets[index];
 
+        Entry<K, V> toRemove = null;
+        for (Entry<K, V> entry : bucket) {
+            if (entry.getKey().equals(key)) {
+                toRemove = entry;
+                break;
+            }
+        }
+
+        if (toRemove != null) {
+            bucket.remove(toRemove);
+        }
     }
 
+
+    /**
+     * Получить хэш код ключа
+     *
+     * @param key ключ
+     * @return int
+     */
     public int getHash(K key) {
         return key == null ? 0 : key.hashCode();
     }
-
 }
